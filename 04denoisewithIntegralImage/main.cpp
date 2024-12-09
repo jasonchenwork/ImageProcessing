6b64ec1d43ab617a86bfe4fce4d1ab94134f7b6c
jason <laevatain103@gmail.com>
2024-12-09 17:49:24 +0800

    03 denoise fix bug (search windows range condition) add 04 denoise with IntergralImage

--- 04denoisewithIntegralImage/main.cpp
+++ 04denoisewithIntegralImage/main.cpp
@@ -357,34 +357,142 @@ void CompareImage(const uint8_t *in1, const uint8_t *in2, uint8_t *out,
     }
   }
 }
-void NLmeans(const uint8_t *imageSource, uint8_t *imageTarget, uint16_t width,
-             uint16_t height, double h, uint8_t halfkernelsize,
-             uint8_t halfsearch) {
+double GetSumFromIntegralImg(double *imageTarget, uint16_t width,
+                             uint16_t height, int16_t x1, int16_t y1,
+                             int16_t x2, int16_t y2) {
+  double sum = imageTarget[y2 * width + x2];
+
+  if (x1 >= 1) {
+    sum -= imageTarget[y2 * width + (x1 - 1)];
+  }
+  if (y1 >= 1) {
+    sum -= imageTarget[(y1 - 1) * width + (x2)];
+  }
+  if (x1 >= 1 && y1 >= 1) {
+    sum += imageTarget[(y1 - 1) * width + (x1 - 1)];
+  }
+
+  return sum;
+}
+void GenDifferIntegralImage(const uint8_t *imageSource, double *imageTarget,
+                            uint16_t width, uint16_t height, int dx, int dy,
+                            int RGB) {
+  double *buf = new double[width * height];
+  for (int i = 0; i < width * height; i++) {
+    imageTarget[i] = 0;
+    buf[i] = 0;
+  }
+  for (int y = 0; y < height; y++) {
+    for (int x = 0; x < width; x++) {
+      int tx = x + dx;
+      int ty = y + dy;
+      if (tx < 0 || tx >= width || ty < 0 || ty >= height) continue;
+      double tmp = (double)imageSource[3 * ((y)*width + (x)) + RGB] -
+                   (double)imageSource[3 * ((ty)*width + (tx)) + RGB];
+
+      buf[y * width + x] = tmp * tmp;
+    }
+  }
+
+  imageTarget[0] = buf[0];
+
+  for (int x = 1; x < width; x++) {
+    imageTarget[x] = buf[x];
+  }
+  for (int y = 1; y < height; y++) {
+    imageTarget[y * width] = buf[y * width];
+  }
+  for (int y = 1; y < height; y++) {
+    for (int x = 1; x < width; x++) {
+      imageTarget[y * width + x] = buf[(y)*width + (x)] +
+                                   imageTarget[y * width + (x - 1)] +
+                                   imageTarget[(y - 1) * width + x] -
+                                   imageTarget[(y - 1) * width + (x - 1)];
+    }
+  }
+  delete[] buf;
+}
+void GenIntegralImage(const uint8_t *imageSource, uint64_t *imageTarget,
+                      uint16_t width, uint16_t height, int RGB) {
+  // int newwidth = width + 1;
+  // int newheight = height + 1;
+
+  imageTarget[0] = imageSource[RGB];
+
+  for (int x = 1; x < width; x++) {
+    (imageTarget)[x] =
+        (imageTarget)[x - 1] + (uint64_t)imageSource[3 * ((x)) + RGB] *
+                                   (uint64_t)imageSource[3 * ((x)) + RGB];
+  }
+  for (int y = 1; y < height; y++) {
+    (imageTarget)[y * width] = (imageTarget)[(y - 1) * width] +
+                               (uint64_t)imageSource[3 * ((y)*width) + RGB] *
+                                   (uint64_t)imageSource[3 * ((y)*width) + RGB];
+  }
+  for (int y = 1; y < height; y++) {
+    for (int x = 1; x < width; x++) {
+      (imageTarget)[y * width + x] =
+          (uint64_t)imageSource[3 * ((y - 1) * width + (x - 1)) + RGB] *
+              (uint64_t)imageSource[3 * ((y - 1) * width + (x - 1)) + RGB] +
+          (imageTarget)[y * width + (x - 1)] +
+          (imageTarget)[(y - 1) * width + x] -
+          (imageTarget)[(y - 1) * width + (x - 1)];
+    }
+  }
+}
+void NLmeanswithIntegralImage(const uint8_t *imageSource, uint8_t *imageTarget,
+                              uint16_t width, uint16_t height, double h,
+                              uint8_t halfkernelsize, uint8_t halfsearch) {
   double h2 = (h * 2.0 + 1) * (h * 2.0 + 1) * 3;
-  // double sigma2 = 5 * 5;
+  double sigma2 = 5 * 5;
 
+  double *sumR = new double[(width) * (height)];
+  double *sumG = new double[(width) * (height)];
+  double *sumB = new double[(width) * (height)];
+  double *weightsumR = new double[(width) * (height)];
+  double *weightsumG = new double[(width) * (height)];
+  double *weightsumB = new double[(width) * (height)];
+  double *weightmaxR = new double[(width) * (height)];
+  double *weightmaxG = new double[(width) * (height)];
+  double *weightmaxB = new double[(width) * (height)];
+  double *integralR = new double[(width) * (height)];
+  double *integralG = new double[(width) * (height)];
+  double *integralB = new double[(width) * (height)];
   for (int x = 0; x < width; ++x) {
-    // cout << "X:" << x << endl;
     for (int y = 0; y < height; ++y) {
-      int w_xmin = max(0, x - halfsearch);
-      int w_xmax = min((int)width, x + halfsearch + 1);
-      int w_ymin = max(0, y - halfsearch);
-      int w_ymax = min((int)height, y + halfsearch + 1);
+      sumR[y * width + x] = 0;
+      sumG[y * width + x] = 0;
+      sumB[y * width + x] = 0;
+      weightsumR[y * width + x] = 0;
+      weightsumG[y * width + x] = 0;
+      weightsumB[y * width + x] = 0;
+      weightmaxR[y * width + x] = 0;
+      weightmaxG[y * width + x] = 0;
+      weightmaxB[y * width + x] = 0;
+    }
+  }
 
-      double sumwR = 0, sumwG = 0, sumwB = 0;
-      double pR = 0, pG = 0, pB = 0;
-      double maxweightR = 0, maxweightG = 0, maxweightB = 0;
+  for (int wx = -(int)halfsearch; wx <= (int)halfsearch; wx++) {
+    for (int wy = -(int)halfsearch; wy <= (int)halfsearch; wy++) {
+      if (wx == 0 && wy == 0) continue;
 
-      for (int wx = w_xmin; wx <= w_xmax; wx++) {
-        for (int wy = w_ymin; wy <= w_ymax; wy++) {
+      GenDifferIntegralImage(imageSource, integralR, width, height, wx, wy, 2);
+      GenDifferIntegralImage(imageSource, integralG, width, height, wx, wy, 1);
+      GenDifferIntegralImage(imageSource, integralB, width, height, wx, wy, 0);
+
+      for (int x = 0; x < width; ++x) {
+        if (x + wx < 0 || x + wx >= width) continue;
+        for (int y = 0; y < height; ++y) {
+          if (y + wy < 0 || y + wy >= height) continue;
+
           double distR = 0, distG = 0, distB = 0;
 
-          if (wx == x && wy == y) continue;
-
+#if 0
+ double distR0 = 0, distG0 = 0, distB0 = 0;
           for (int kx = -halfkernelsize; kx <= halfkernelsize; kx++) {
             for (int ky = -halfkernelsize; ky <= halfkernelsize; ky++) {
               int nx = x + kx, ny = y + ky;
-              int nwx = wx + kx, nwy = wy + ky;
+              int nwx = wx + nx, nwy = wy + ny;
 
               if (nx < 0 || nx >= width || ny < 0 || ny >= height || nwx < 0 ||
                   nwx >= width || nwy < 0 || nwy >= height)
@@ -401,54 +509,84 @@ void NLmeans(const uint8_t *imageSource, uint8_t *imageTarget, uint16_t width,
               int targetG = *(imageSource + targetIndex + 1);
               int targetB = *(imageSource + targetIndex + 0);
 
-              distR += (curR - targetR) * (curR - targetR);
-              distG += (curG - targetG) * (curG - targetG);
-              distB += (curB - targetB) * (curB - targetB);
+              distR0 += (curR - targetR) * (curR - targetR);
+              distG0 += (curG - targetG) * (curG - targetG);
+              distB0 += (curB - targetB) * (curB - targetB);
             }
           }
+#else
 
-          distR /= (2 * halfkernelsize + 1) * (2 * halfkernelsize + 1);
-          distG /= (2 * halfkernelsize + 1) * (2 * halfkernelsize + 1);
-          distB /= (2 * halfkernelsize + 1) * (2 * halfkernelsize + 1);
-
+          distR = GetSumFromIntegralImg(integralR, width, height,
+                                        (int16_t)x - (int16_t)halfkernelsize,
+                                        (int16_t)y - (int16_t)halfkernelsize,
+                                        (int16_t)x + (int16_t)halfkernelsize,
+                                        (int16_t)y + (int16_t)halfkernelsize);
+          distG = GetSumFromIntegralImg(integralG, width, height,
+                                        (int16_t)x - (int16_t)halfkernelsize,
+                                        (int16_t)y - (int16_t)halfkernelsize,
+                                        (int16_t)x + (int16_t)halfkernelsize,
+                                        (int16_t)y + (int16_t)halfkernelsize);
+          distB = GetSumFromIntegralImg(integralB, width, height,
+                                        (int16_t)x - (int16_t)halfkernelsize,
+                                        (int16_t)y - (int16_t)halfkernelsize,
+                                        (int16_t)x + (int16_t)halfkernelsize,
+                                        (int16_t)y + (int16_t)halfkernelsize);
+#endif
+#if 1
+          distR /= (2.0 * halfkernelsize + 1.0) * (2.0 * halfkernelsize + 1.0);
+          distG /= (2.0 * halfkernelsize + 1.0) * (2.0 * halfkernelsize + 1.0);
+          distB /= (2.0 * halfkernelsize + 1.0) * (2.0 * halfkernelsize + 1.0);
+#endif
 #if 0
           distR = max(distR - 2 * sigma2, 0.0);
           distG = max(distG - 2 * sigma2, 0.0);
           distB = max(distB - 2 * sigma2, 0.0);
 #endif
+
           double weightR = exp(-distR / h2);
           double weightG = exp(-distG / h2);
           double weightB = exp(-distB / h2);
 
-          int targetIndex = 3 * (width * wy + wx);
-          pR += weightR * (*(imageSource + targetIndex + 2));
-          pG += weightG * (*(imageSource + targetIndex + 1));
-          pB += weightB * (*(imageSource + targetIndex + 0));
+          int targetIndex = 3 * (width * (wy + y) + (wx + x));
 
-          sumwR += weightR;
-          sumwG += weightG;
-          sumwB += weightB;
+          sumR[y * width + x] += weightR * (*(imageSource + targetIndex + 2));
+          sumG[y * width + x] += weightG * (*(imageSource + targetIndex + 1));
+          sumB[y * width + x] += weightB * (*(imageSource + targetIndex + 0));
 
-          if (weightR > maxweightR) maxweightR = weightR;
-          if (weightG > maxweightG) maxweightG = weightG;
-          if (weightB > maxweightB) maxweightB = weightB;
+          weightsumR[y * width + x] += weightR;
+          weightsumG[y * width + x] += weightG;
+          weightsumB[y * width + x] += weightB;
+
+          weightmaxR[y * width + x] = max(weightmaxR[y * width + x], weightR);
+          weightmaxG[y * width + x] = max(weightmaxG[y * width + x], weightG);
+          weightmaxB[y * width + x] = max(weightmaxB[y * width + x], weightB);
         }
       }
+    }
+  }
+  for (int x = 0; x < width; ++x) {
+    for (int y = 0; y < height; ++y) {
       double curR = *(imageSource + 3 * (width * (y) + x) + 2);
       double curG = *(imageSource + 3 * (width * (y) + x) + 1);
       double curB = *(imageSource + 3 * (width * (y) + x) + 0);
 
-      pR += curR * maxweightR;
-      pG += curG * maxweightG;
-      pB += curB * maxweightB;
+      sumR[y * width + x] += (double)curR * weightmaxR[y * width + x];
+      sumG[y * width + x] += (double)curG * weightmaxG[y * width + x];
+      sumB[y * width + x] += (double)curB * weightmaxB[y * width + x];
 
-      sumwR += maxweightR;
-      sumwG += maxweightG;
-      sumwB += maxweightB;
+      weightsumR[y * width + x] += weightmaxR[y * width + x];
+      weightsumG[y * width + x] += weightmaxG[y * width + x];
+      weightsumB[y * width + x] += weightmaxB[y * width + x];
 
-      double newvalueR = (sumwR > 0) ? (pR / sumwR) : 0;
-      double newvalueG = (sumwG > 0) ? (pG / sumwG) : 0;
-      double newvalueB = (sumwB > 0) ? (pB / sumwB) : 0;
+      double newvalueR = (weightsumR[y * width + x] > 0)
+                             ? (sumR[y * width + x] / weightsumR[y * width + x])
+                             : 0;
+      double newvalueG = (weightsumG[y * width + x] > 0)
+                             ? (sumG[y * width + x] / weightsumG[y * width + x])
+                             : 0;
+      double newvalueB = (weightsumB[y * width + x] > 0)
+                             ? (sumB[y * width + x] / weightsumB[y * width + x])
+                             : 0;
 
       if (newvalueR > 255) newvalueR = 255;
       if (newvalueR < 0) newvalueR = 0;
@@ -462,6 +600,21 @@ void NLmeans(const uint8_t *imageSource, uint8_t *imageTarget, uint16_t width,
       *(imageTarget + 3 * (width * (y) + x) + 0) = (uint8_t)newvalueB;
     }
   }
+  delete[] integralR;
+  delete[] integralG;
+  delete[] integralB;
+
+  delete[] sumR;
+  delete[] sumG;
+  delete[] sumB;
+
+  delete[] weightsumR;
+  delete[] weightsumG;
+  delete[] weightsumB;
+
+  delete[] weightmaxR;
+  delete[] weightmaxG;
+  delete[] weightmaxB;
 }
 
 void testRWbmp() {
@@ -494,7 +647,8 @@ void testDeNoise() {
 
   SimpleImage *dstimg = new SimpleImage(srcimg->width, srcimg->height);
   clock_gettime(CLOCK_REALTIME, &t_start);
-  NLmeans(srcimg->image, dstimg->image, srcimg->width, srcimg->height, 5, 3, 5);
+  NLmeanswithIntegralImage(srcimg->image, dstimg->image, srcimg->width,
+                           srcimg->height, 5, 3, 5);
   clock_gettime(CLOCK_REALTIME, &t_end);
   elapsedTime = (t_end.tv_sec - t_start.tv_sec) * 1000.0;
   elapsedTime += (t_end.tv_nsec - t_start.tv_nsec) / 1000000.0;
